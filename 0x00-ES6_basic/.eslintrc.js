8e488963a2937772ae492d2943471b6ba75b7096
hassankyanzi <kyanzi.hassan2@gmail.com>
2024-07-02 11:58:52 +0300

    change  es6 data manipulation

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: 'airbnb-base',
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      // You can customize ESLint rules here if needed
+    },
+  };
+  
